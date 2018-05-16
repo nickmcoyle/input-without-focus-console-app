8eb679814c4a48cb4faaab10e03451b2ba863543
Nick <nickmcoyle@gmail.com>
2018-05-15 18:19:40 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        const UInt32 WM_KEYDOWN = 0x0100;
+        const int VK_F5 = 0x74;
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+        [STAThread]
+        static void Main()
+        {
+            while (true)
+            {
+                Process[] processes = Process.GetProcessesByName("notepad");
+
+                foreach (Process proc in processes)
+                 PostMessage(proc.MainWindowHandle, WM_KEYDOWN, VK_F5, 0);               
+
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
+
